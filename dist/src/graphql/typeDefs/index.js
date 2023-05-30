b0cae5a6d6d1ffd72e676c0ff38b63b51dfea13b
moby76 <moby76@mail.ru>
2023-05-30 23:05:10 +0700

    Delete dist directory

--- dist/src/graphql/typeDefs/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const merge_1 = require("@graphql-tools/merge");
-const userTypeDefs_1 = require("./userTypeDefs");
-const conversationTypeDefs_1 = require("./conversationTypeDefs");
-const messageTypeDefs_1 = require("./messageTypeDefs");
-const typeDefs = (0, merge_1.mergeTypeDefs)([userTypeDefs_1.userTypes, conversationTypeDefs_1.conversationTypes, messageTypeDefs_1.messageTypes]);
-exports.default = typeDefs;
